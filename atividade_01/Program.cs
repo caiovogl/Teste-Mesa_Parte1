91fab2200a8b978d7d5583d704a11663f3e4ee40
caiovogl <58825693+caiovogl@users.noreply.github.com>
2023-09-13 17:31:58 -0300

    Update Program.cs

--- atividade_01/Program.cs
+++ atividade_01/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 using System;
 
 //Problema 1
@@ -20,21 +20,20 @@ Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
 
 //Problema 2
 
+Console.WriteLine("Problema 2:");
+
 a = 2;
 while(a<6){
     v[a] = a*10;
+    Console.WriteLine(v[a]);
     a++;
-}
-
-Console.WriteLine("Problema 2:");
-
-for(int i = 0;i<7;i++){
-    Console.WriteLine(v[i]);
 }
 
 Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
 
 //Problema 3
+
+Console.WriteLine("Problema 3:");
 
 v = new int[10];
 
@@ -42,12 +41,7 @@ a = 7;
 b = a-6;
 while(b<a){
     v[b] = a+b;
+    Console.WriteLine(v[b]);
     b+=2;
-}
-
-Console.WriteLine("Problema 3:");
-
-for(int i = 0;i<8;i++){
-    Console.WriteLine(v[i]);
 }
 
